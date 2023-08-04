17e5194cd68f4b76101b0ebc6e9aadd425a7e8e0
StephanyKleen <stephanykleen@gmail.com>
2023-08-03 21:17:05 -0700

    Practice Timing Benchmarks and Scaling

--- phase-2.js
+++ phase-2.js
@@ -2,13 +2,23 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let sum = [];
+    for(let i = 1; i <= 10; i++){
+      let increase = i * increment
+      sum.push(addNums(increase));
+    }
+  return sum;
 
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+    let sum = [];
+    for(let i = 1; i <= 10; i++){
+      let increase = i * increment
+      sum.push(addManyNums(increase));
+    }
+  return sum;
 
 }
 
